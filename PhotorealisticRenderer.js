99eb36d2f521f58c77d31e5f1658a6acf832819e
Robert <robertsarvas@gmail.com>
2015-01-30 22:29:12 +0100

    reapir the code

--- PhotorealisticRenderer.js
+++ PhotorealisticRenderer.js
@@ -6,23 +6,23 @@ function PhotorealisticRenderer(domQuery) {
     var self = BasicThreeRenderer(domQuery);
         
     self.shadowMapEnabled = true;
-    
+    self.scene = null;
 
 
-    var scene = new THREE.Scene();    
+    var self.scene = new THREE.Scene();    
 
     //Plane for testing in case there are some problems receiving or processing the server data
        var plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshNormalMaterial());
        plane.position.z = -100;
        plane.position.y = -100;
        plane.overdraw = true;
-       scene.add(plane);
+       self.scene.add(plane);
             
     var ambientLight = new THREE.AmbientLight(0x0c0c0c);
-    	scene.add(ambientLight);
+    	self.scene.add(ambientLight);
 
     var spotLight = new THREE.SpotLight (0xffffff);
-    	scene.add( spotLight);     
+    	self.scene.add( spotLight);     
 
   return self;
 }
