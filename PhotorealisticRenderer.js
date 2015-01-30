6c5005ccdc6e5539aa55f5659fcb2a66ba5057de
Robert <robertsarvas@gmail.com>
2015-01-30 22:24:25 +0100

    add ambientlight and spot light

--- PhotorealisticRenderer.js
+++ PhotorealisticRenderer.js
@@ -9,26 +9,20 @@ function PhotorealisticRenderer(domQuery) {
     
 
 
-    self.scene = new THREE.Scene();    
+    var scene = new THREE.Scene();    
 
     //Plane for testing in case there are some problems receiving or processing the server data
-       var plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), new THREE.MeshNormalMaterial());
+       var plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshNormalMaterial());
        plane.position.z = -100;
        plane.position.y = -100;
        plane.overdraw = true;
        self.scene.add(plane);
             
-            
-    THREE.AmbientLight = function ( color ) {
-	   THREE.Light.call( self, color );
-	   self.type = 'AmbientLight';
-    };
+    var ambientLight = new THREE.AmbientLight(0x0c0c0c);
+    	scene.add(ambientLight);
 
-    THREE.AmbientLight.prototype = Object.create( THREE.Light.prototype );
-       THREE.AmbientLight.prototype.clone = function () {
-       var light = new THREE.AmbientLight();
-       THREE.Light.prototype.clone.call( self, light );
-       return light;
-     };
+    var spotLight = new THREE.SpotLight (0xffffff);
+    	scene.add( spotLight);     
+
   return self;
 }
