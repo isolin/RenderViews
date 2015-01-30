9b8c0d8ea61a1fe18b60ffe815be5d6db2d98296
Robert <robertsarvas@gmail.com>
2015-01-30 22:31:01 +0100

    repair bad code

--- PhotorealisticRenderer.js
+++ PhotorealisticRenderer.js
@@ -9,7 +9,8 @@ function PhotorealisticRenderer(domQuery) {
     self.scene = null;
 
 
-    var self.scene = new THREE.Scene();    
+    self.scene = new THREE.Scene(); 
+        
 
     //Plane for testing in case there are some problems receiving or processing the server data
        var plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshNormalMaterial());
