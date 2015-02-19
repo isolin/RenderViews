d85bad7bdda31c560114cb02e42b2fde0778ea24
Robert <robertsarvas@gmail.com>
2015-02-20 00:39:27 +0100

    code in dev was bad,, cop past code from master

--- PhotorealisticRenderer.js
+++ PhotorealisticRenderer.js
@@ -5,31 +5,48 @@ function PhotorealisticRenderer(domQuery) {
 
     var self = BasicThreeRenderer(domQuery);
         
-    self.shadowMapEnabled = true;
-    self.scene = null;
     self.renderer = null;
+    self.scene = null;
     self.camera = null;
+    self.shadowMapEnabled = true;
+        
+ 
 
-    self.renderer = new THREE.WebGL.Renderer();
-    self.renderer.setSize(window.innerWidth,window.innerHeight);
-    self.renderer.setClearColor(0xffffff, 1); //white)
-    doument.body.appendChild(self.renderer.domElement);
+    //Sets the renderer window size to the size of the container where it will be attached
+      this.renderer.setSize(this.container.innerWidth(), this.container.innerHeight());
+      this.container.append(this.renderer.domElement); //attaches the <canvas> tag for the renderer as a child to the container element
+
+    //Camera initialization
+      this.camera = new THREE.PerspectiveCamera(50, this.container.innerWidth() / this.container.innerHeight(), 0.1, 1000); //field of view angle, aspect ratio, near clipping plane, far clipping plane
+      //position of the camera
+      this.camera.position.y = 5;
+	  this.camera.position.z = 4;
+	  this.camera.rotation.x = 45 * (Math.PI / 180);
 
 
-    self.scene = new THREE.Scene(); 
+      //Background of the canvas
+        this.renderer.setClearColorHex(0xffffff, 1); //white
+        //Our main scene
+        this.scene = new THREE.Scene();
 
-    //Plane for testing in case there are some problems receiving or processing the server data
-       var plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(100, 100), new THREE.MeshNormalMaterial());
-       plane.position.z = -100;
-       plane.position.y = -100;
-       plane.overdraw = true;
-       self.scene.add(plane);
+      //Plane for testing in case there are some problems receiving or processing the server data
+         var plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), new THREE.MeshNormalMaterial());
+       	 plane.position.z = -100;
+         plane.position.y = -100;
+         plane.overdraw = true;
+         this.scene.add(plane);
             
-    var ambientLight = new THREE.AmbientLight(0x0c0c0c);
-    	self.scene.add(ambientLight);
+            
+      THREE.AmbientLight = function ( color ) {
+	     THREE.Light.call( this, color );
+	     this.type = 'AmbientLight';
+      };
 
-    var spotLight = new THREE.SpotLight (0xffffff);
-    	self.scene.add( spotLight);     
-
-  return self;
+      THREE.AmbientLight.prototype = Object.create( THREE.Light.prototype );
+         THREE.AmbientLight.prototype.clone = function () {
+         var light = new THREE.AmbientLight();
+         THREE.Light.prototype.clone.call( this, light );
+         return light;
+        }
+    return self;
 }
