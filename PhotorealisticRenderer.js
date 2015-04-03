d040ff5d441d90ec7919708ea191bd3bca529c01
Robert <robertsarvas@gmail.com>
2015-04-03 23:30:15 +0200

    cccc

--- PhotorealisticRenderer.js
+++ PhotorealisticRenderer.js
@@ -15,7 +15,7 @@ function PhotorealisticRenderer(domQuery) {
         self.renderer.shadowMapEnabled = true;      
 
         //Camera initialization
-        self.camera = new THREE.PerspectiveCamera(50, 19, 0.1, 1000); //field of view angle, aspect ratio, near clipping plane, far clipping plane
+        self.camera = new THREE.PerspectiveCamera(50, this.container.innerWidth() / this.container.innerHeight(), 0.1, 1000); //field of view angle, aspect ratio, near clipping plane, far clipping plane
         //position of the camera
         self.camera.position.y = 5;
 	      self.camera.position.z = 4;
