a9101e980e044a6bd649f9c17e0cb834a9319256
Robert <robertsarvas@gmail.com>
2015-04-04 01:14:40 +0200

    rwarfe

--- PhotorealisticRenderer.js
+++ PhotorealisticRenderer.js
@@ -25,13 +25,13 @@ function PhotorealisticRenderer(domQuery) {
         self.scene = new THREE.Scene();
 
         // Plane for testing in case there are some problems receiving or processing the server data
-        var plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), new THREE.MeshNormalMaterial());
+        var plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshNormalMaterial());
        	plane.position.z = -100;
         plane.position.y = -100;
         plane.overdraw = true;
 
         //var ambiColor = "#0c0c0c";            
-    	  var light = new THREE.AmbientLight(0x404040);
+    	  //var light = new THREE.AmbientLight(0x404040);
         var spotLight = new THREE.SpotLight( 0xffffff );
         spotLight.position.set( -40, 60, -10 );
 
