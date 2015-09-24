d7ac3179c06a37698db7988eadd9dfe080fdbc67
Robert <robertsarvas@gmail.com>
2015-09-24 23:20:03 +0200

    Update

--- PhotorealisticRenderer.js
+++ PhotorealisticRenderer.js
@@ -1,50 +1,116 @@
-function PhotorealisticRenderer(domQuery) {
+  function PhotorealisticRenderer(domQuery) {
 
 // definition of my class PhotorealisticRenderer
 
 
     var self = BasicThreeRenderer(domQuery);
+        
+    self.renderer = null;
+    self.scene = null;
+    self.camera = null;
+
+    var DirLight, SpotLight, stats;
+    var cube;
+      
+    init();
+    animate();  
+ 
+    function init() {
+          //Sets the renderer window size to the size of the container where it will be attached
+          this.renderer.setSize(this.container.innerWidth(), this.container.innerHeight());
+          this.container.append(this.renderer.domElement); //attaches the <canvas> tag for the renderer as a child to the container element
+
+          //Camera initialization
+          this.camera = new THREE.PerspectiveCamera(50, this.container.innerWidth() / this.container.innerHeight(), 0.1, 1000); //field of view angle, aspect ratio, near clipping plane, far clipping plane
+          //position of the camera
+          this.camera.position.set( 0, 15, 35 );
+
+
+          //Background of the canvas
+          this.renderer.setClearColorHex(0xffffff, 1); //white
+          //Our main scene
+          this.scene = new THREE.Scene();
+
+          //Plane for testing in case there are some problems receiving or processing the server data
+          var plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), new THREE.MeshNormalMaterial());
+          plane.position.z = -100;
+          plane.position.y = -100;
+          plane.overdraw = true;
+          this.scene.add(plane);
+
+          //LIGHTS
+            
+          var ambient = new THREE.AmbientLightt( 0x404040 );
+          this.scene.add( ambient );
+
+          spotLight = new THREE.SpotLight( 0xffffff );
+          spotLight.position.set( 10, 10, 5 );
+          spotLight.castShadow = true;
+          spotLight.shadowCameraNear = 8;
+          spotLight.shadowCameraFar = 30;
+          spotLight.shadowDarkness = 0.5;
+          spotLight.shadowCameraVisible = true;
+          spotLight.shadowMapWidth = 1024;
+          spotLight.shadowMapHeight = 1024;
+          spotLight.name = 'Spot Light';
+          scene.add( spotLight );
+
+          dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+          dirLight.position.set( 0, 10, 0 );
+          dirLight.castShadow = true;
+          dirLight.shadowCameraNear = 0.01;
+          dirLight.shadowCameraFar = 10;
+          dirLight.shadowCameraRight = 15;
+          dirLight.shadowCameraLeft = -15;
+          dirLight.shadowCameraTop  = 15;
+          dirLight.shadowCameraBottom = -15;
+          dirLight.shadowDarkness = 0.5;
+          dirLight.shadowCameraVisible = true;
+          dirLight.shadowMapWidth = 1024;
+          dirLight.shadowMapHeight = 1024;
+          dirLight.name = 'Dir. Light';
+          scene.add( dirLight );
+
+          var geometry = new THREE.BoxGeometry( 3, 3, 3 );
+          cube = new THREE.Mesh( geometry, material );
+          cube.position.set( 8, 3, 8 );
+          cube.castShadow = true;
+          cube.receiveShadow = true;
+          scene.add( cube );
+
+          var geometry = new THREE.BoxGeometry( 10, 0.15, 10 );
+          var material = new THREE.MeshPhongMaterial( {
+            color: 0xa0adaf,
+            shininess: 150,
+            specular: 0xffffff,
+            shading: THREE.SmoothShading
+          } );
+
+          var ground = new THREE.Mesh( geometry, material );
+          ground.scale.multiplyScalar( 3 );
+          ground.castShadow = false;
+          ground.receiveShadow = true;
+          scene.add( ground );
+
+      }
+          
+    function animate() {
+
+        requestAnimationFrame( animate );
+        render();
+
+      }
+
+    function render() {
+          var time = Date.now() * 0.0005;
+
+          if ( mesh ) mesh.rotation.y -= 0.01;
+
+
+          renderer.render( scene, camera );
+
+      }
     
-    self.initCalls.push( function (){
-        this.projector = new THREE.Projector();
-        this.renderer.shadowMapEnabled = true;
-        this.renderer.setClearColor(0xffffff, 1); //white
-      });    
-   
-    // set the renderer
-        //self.renderer = new THREE.WebGLRenderer();
-        //self.renderer.setClearColor(0xffffff, 1); //white
-        //self.renderer.shadowMapEnabled = true;      
-
-        //Camera initialization
-        //self.camera = new THREE.PerspectiveCamera(50, 1920/1080, 0.1, 1000); //field of view angle, aspect ratio, near clipping plane, far clipping plane
-        //position of the camera
-        //self.camera.position.y = 5;
-	      //self.camera.position.z = 4;
-	      //self.camera.rotation.x = 45 * (Math.PI / 180);
-
-        //Our main scene
-        self.scene = new THREE.Scene();
-
-        // Plane for testing in case there are some problems receiving or processing the server data
-        var plane = new THREE.Mesh(new THREE.PlaneGeometry(50, 50), new THREE.MeshNormalMaterial());
-       	plane.position.z = -100;
-        plane.position.y = -100;
-        plane.overdraw = true;
-
-        //var ambiColor = "#0c0c0c";            
-    	  var light = new THREE.AmbientLight(0x404040);
-        var spotLight = new THREE.SpotLight( 0xffffff );
-        spotLight.position.set( -40, 60, -10 );
-
-
-    self.updateCalls.push(function () {
-        self.scene.add( spotLight );
-        //self.scene.add(light);
-        self.scene.add(plane);
-        //self.camera.rotation.x = 15 * (Math.PI / 180);
-        //this.renderer.render(this.scene, this.camera);
-    });
 
     return self;
-}
+  }
